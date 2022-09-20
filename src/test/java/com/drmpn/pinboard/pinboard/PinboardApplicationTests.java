474bc94c11bde0790cb7e1133b729c54215f00b7
DRMPN <mr.gorden.smit@gmail.com>
2022-09-20 21:06:59 +0300

    initial commit

---
+++ src/test/java/com/drmpn/pinboard/pinboard/PinboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drmpn.pinboard.pinboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
